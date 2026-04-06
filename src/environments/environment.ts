2039a7f446dda62ebfb9d82f2018779a2e189c1e
Baruch Ginzburg <ginzburgbaruch@gmail.com>
2026-04-06 14:43:20 +0300

    favicon added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
-import { environmentSecrets } from './environment.secret';
-
 export const environment = {
   production: false,
   spotifyClientId: '59eaaca39450479c9d97fd2da47ac4c5',
-  spotifyClientSecret: environmentSecrets.spotifyClientSecret
+  spotifyClientSecret: '' // Locally, you can paste your secret here or use the secret file.
 };
