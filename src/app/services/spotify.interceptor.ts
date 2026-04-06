7b4b1fa022b2700cd185fc693529df811a68f6c5
Baruch Ginzburg <ginzburgbaruch@gmail.com>
2026-04-06 12:19:20 +0300

    external link to spotify added

--- src/app/services/spotify.interceptor.ts
+++ src/app/services/spotify.interceptor.ts
@@ -1,7 +1,7 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
-import { switchMap, from } from 'rxjs';
+import { switchMap, from, catchError, throwError } from 'rxjs';
 
 export const spotifyInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
@@ -18,7 +18,23 @@ export const spotifyInterceptor: HttpInterceptorFn = (req, next) => {
     const clonedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
-    return next(clonedReq);
+    return next(clonedReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // If Spotify explicitly rejects the token as expired (401)
+        if (error.status === 401) {
+          return from(authService.fetchToken()).pipe(
+            switchMap(() => {
+              const refreshedToken = authService.accessToken();
+              const retryReq = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${refreshedToken}`)
+              });
+              return next(retryReq);
+            })
+          );
+        }
+        return throwError(() => error);
+      })
+    );
   }
 
   // If we don't have a token yet (e.g. initial bootup), await the fetchToken promise before sending request
